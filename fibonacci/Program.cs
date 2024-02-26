dcb3672acf5e8bd5ab4bdb00c1d11882b88ac9cb
scorp <scorp@DESKTOP-72D584C>
2024-02-26 20:32:17 +0200

    Add project files.

---
+++ fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int limit = 4000000; // Set your desired limit
+        Console.WriteLine("Sum of even-valued terms in Fibonacci sequence up to " + limit + ":");
+
+        int sum = 0;
+        int currentTerm = 1;
+        int nextTerm = 2;
+
+        while (currentTerm <= limit)
+        {
+            if (currentTerm % 2 == 0)
+            {
+                sum += currentTerm;
+            }
+
+            int temp = currentTerm + nextTerm;
+            currentTerm = nextTerm;
+            nextTerm = temp;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
